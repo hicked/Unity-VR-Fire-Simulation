05b2674a773e8f5ec0df5134280ac2adf5297891
Antoine Hickey <AntoineHickey@cunet.carleton.ca>
2024-05-09 14:23:02 -0400

    updated physics

--- Software/Custom Game Assets/Scripts/Player.cs
+++ Software/Custom Game Assets/Scripts/Player.cs
@@ -6,16 +6,17 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float baseSpeedMultiplier = 2f;
     [SerializeField] private float sprintSpeedMultiplier = 3.5f;
-    [SerializeField] private float playerCollisionRadius = 0.2f;
-    [SerializeField] private float playerHeight = 2f;
+    
+    private BoxCollider playerHitBox;
     [SerializeField] private float maxDistance = 0.2f;
-    [SerializeField] private LayerMask layerForCollisions; 
+    [SerializeField] private LayerMask layersForCollisions; 
+    [SerializeField] private float minHeightBeforeCollisions = 0.1f;  // Note this is the height from the top, so well have to do some arithmetic to convert it into the minimum height from the bottom
 
-
-    private void start() {
+    private void Start() {
         if (!(transform.localScale.x == transform.localScale.z)) {
-            throw new UnityException("Player collider must be cylindrical and therefore x scale and z scale must be the same");
+            throw new UnityException("Player collider x scale and z scale must be the same");
         }
+        playerHitBox = GetComponent<BoxCollider>();
     }
     void Update() {
         Vector3 inputVector = new Vector3(0f, 0f, 0f);
@@ -41,15 +42,23 @@ public class Player : MonoBehaviour
     }
     
     public bool canMove(Vector3 dir) {
-        return !Physics.CapsuleCast(
-            transform.position, 
-            transform.position + Vector3.up * playerHeight, 
-            playerCollisionRadius, 
-            dir, 
-            maxDistance, 
-            layerForCollisions
-        );
+        float minHeightBeforeCollisionsFromBottom = playerHitBox.size.y - minHeightBeforeCollisions;
+        //Debug.Log(minHeightBeforeCollisionsFromBottom);
+        RaycastHit hitInfo;
 
+        bool hit = !Physics.BoxCast(
+            new Vector3(transform.position.x, 
+                        transform.position.y - playerHitBox.size.y + (playerHitBox.size.y - minHeightBeforeCollisions)/2f + minHeightBeforeCollisions, 
+                        transform.position.z), 
+            new Vector3((playerHitBox.size.x - maxDistance)/2f, (playerHitBox.size.y - minHeightBeforeCollisions)/2f, (playerHitBox.size.z - maxDistance)/2f), // IMOPRTANT: set the y to negative, that way its starts from the top down instead of bottom up
+            dir, 
+            out hitInfo,
+            transform.rotation, 
+            maxDistance,
+            layersForCollisions
+        );
+        Debug.DrawRay(transform.position, dir * maxDistance, hit ? Color.red : Color.green);
+        return hit;
     }
 }
 
