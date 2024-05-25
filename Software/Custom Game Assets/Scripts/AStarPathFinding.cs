10b461f4139e358a7a74e4e85f5753ccaa7c7ecc
Antoine Hickey <AntoineHickey@cunet.carleton.ca>
2024-05-25 14:42:09 -0400

    A*

--- Software/Custom Game Assets/Scripts/AStarPathFinding.cs
+++ Software/Custom Game Assets/Scripts/AStarPathFinding.cs
@@ -1,36 +1,19 @@
-using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AStarPathfinding;
+using UnityEngine;
 
-namespace AStarPathfinding {
+public class AStarPathfinding : MonoBehaviour {
     [SerializeField] private float NPCHeight = 0.2f;
-    [SerializeField] private float tileSize = 1f;
-
-    class Location {
-        public float x;
-        public float y;
-        public float z;
-        public float g; // Cost to get to location (from start) REAL
-        public float h; // Cost to get to end (from current location)
-        public float f;
-
-        // Constructor with parameters
-        public void Location(Vector3 current, Vector3 start, Vector3 end) {
-            this.x = current.x;
-            this.y = current.y;
-            this.z = current.z;
-            this.g = Mathf.Abs(current.x - start.x) + Mathf.Abs(current.z - start.z);
-            this.h = Mathf.Abs(current.x - end.x) + Mathf.Abs(current.z - end.z);
-            this.f = this.g + this.h;
-        }
-    }
+    [SerializeField] private static float tileSize = 1f; // square meters
+    [SerializeField] private float buffer = 3f;
+    private RaycastHit hitInfo;
 
     private void Start() {
-        foreach (Location location in findPath(transform.position, new Vector3(0, 0, 0))) {
-            Debug.Log(location);
+        List<Location> qwerty = findPath(transform.position, new Vector3(3.5f, 0, -20f));
+
+        Debug.Log(qwerty.Count);
+        foreach (Location location in qwerty) {
+            Debug.Log(new Vector3(location.x, location.y, location.z));
         }
     }
 
@@ -41,10 +24,22 @@ namespace AStarPathfinding {
     public List<Location> findPath(Vector3 Start, Vector3 End) {
         List<Location> path = new List<Location>();
         
-        Location currentLocation = new Location(Start, Start, End); // current location initialized as starting location
-        List<Location> validNeighbors = getValidNeighbors(currentLocation, Start, End);
-        
-        while ((int)currentLocation.x != (int)End.x && (int)currentLocation.y != (int)End.y && (int)currentLocation.z != (int)End.z) {
+        Location start = new Location(Start, Start, End);
+        Location end = new Location(End, Start, End);
+
+        Location currentLocation = start; // current location initialized as starting location
+        List<Location> validNeighbors = getValidNeighbors(currentLocation, start, end);
+        int bla = 0;
+        while (Vector3.Distance(new Vector3(currentLocation.x, currentLocation.y, currentLocation.z), End) > tileSize) {
+            bla++;
+            if (bla > 50) {
+                break;
+            }
+            if (validNeighbors.Count == 0) {
+                // No valid path found, terminate the loop
+                break;
+            }
+
             Location bestLocation = null;
             for (int i = 0; i < (validNeighbors.Count); i++) {
                 if (i == 0) { // if it is the first neightbor, just assume its the best one so far
@@ -53,12 +48,14 @@ namespace AStarPathfinding {
                 
                 else if (validNeighbors[i].f < bestLocation.f) {
                     bestLocation = validNeighbors[i];
+
+                    Debug.Log(bestLocation.h);
                 }
-            }   
+            }
             path.Add(bestLocation);
             
             currentLocation = bestLocation;
-            validNeighbors = getValidNeighbors(currentLocation, Start, End);
+            validNeighbors = getValidNeighbors(currentLocation, start, end);
         }
         return path;
     }
@@ -68,17 +65,17 @@ namespace AStarPathfinding {
         List<Location> validNeighbors = new List<Location>();
 
         // Define offsets for neighboring locations (up, down, left, right)
-        Vector2Int[] directions = {
-            new Vector3(1, 0, 0),
-            new Vector3(-1, 0, 0),            
-            new Vector3(0, 0, 1),
-            new Vector3(0, 0, -1)
+        Vector3[] directions = {
+            new Vector3(1f, 0f, 0f),
+            new Vector3(-1f, 0f, 0f),            
+            new Vector3(0f, 0f, 1f),
+            new Vector3(0f, 0f, -1f)
             };
 
         // Perform raycasts to check for obstacles and add valid neighbors
         foreach (Vector3 direction in directions) {
-            if (!isBlocked(location, direction, tileSize)) {
-                validNeighbors.Add(new Location(location + direction*tileSize, start, end));
+            if (!isBlocked(new Vector3(location.x, location.y, location.z), direction, tileSize)) {
+                validNeighbors.Add(new Location(new Vector3(location.x, location.y, location.z) + direction*tileSize, new Vector3(start.x, start.y, start.z), new Vector3(end.x, end.y, end.z)));
             }
         }
 
@@ -96,5 +93,24 @@ namespace AStarPathfinding {
             out hitInfo,
             distance);
         return hit;
+    }
+
+    public class Location {
+        public float x;
+        public float y;
+        public float z;
+        public float g; // Cost to get to location (from start) REAL
+        public float h; // Cost to get to end (from current location)
+        public float f;
+
+        // Constructor with parameters
+        public Location(Vector3 current, Vector3 start, Vector3 end) {
+            this.x = current.x;
+            this.y = current.y;
+            this.z = current.z;
+            this.g = (Mathf.Abs(current.x - start.x) + Mathf.Abs(current.z - start.z))/tileSize; // sub this for a passed value of g
+            this.h = (Mathf.Abs(current.x - end.x) + Mathf.Abs(current.z - end.z))/tileSize;
+            this.f = this.g + this.h;
+        }
     }
 }
