2165c0b52e32a68fc0dce5bae0274d050b403e81
Antoine Hickey <antoinehickey123@gmail.com>
2024-08-07 10:48:29 -0400

    modified arduinoIO

--- Hardware/core/Main/Main.ino
+++ Hardware/core/Main/Main.ino
@@ -1,27 +1,37 @@
 void setup() {
   Serial.begin(19600);
   pinMode(2, OUTPUT); // LED
-  pinMode(3, INPUT); // button
-  // should be a gyroscope so it wont be a singular button
+  pinMode(3, INPUT); // Left Wheel
+  pinMode(4, INPUT); // Right Wheel
 }
 
 
 const int BUFFER_SIZE = 50; // max size of data being pushed through
-char inputData[BUFFER_SIZE];
+float leftSpeed = 0;
+float rightSpeed = 0;
+float data[2] = {0, 0};
 
 void loop() {
   if (Serial.available() > 0) {
-    int dataLen = Serial.readBytes(inputData, BUFFER_SIZE);
+    leftSpeed = digitalRead(3);
+    rightSpeed = digitalRead(4);
+    data[0] = leftSpeed;
+    data[1] = rightSpeed;
 
-    if (inputData[0] == '0') {
-      digitalWrite(2, LOW);
-    }
-    else if (inputData[0] == '1') {
-      digitalWrite(2, HIGH);
-    }
+    // Convert float array to byte array
+    byte byteData[sizeof(data)];
+    memcpy(byteData, data, sizeof(data));
+
+    // Send the byte array through Serial
+    int dataLen = Serial.write(byteData, sizeof(byteData));
+
+    // if (inputData[0] == '0') {
+    //   digitalWrite(2, LOW);
+    // }
+    // else if (inputData[0] == '1') {
+    //   digitalWrite(2, HIGH);
+    // }
   }
 
-
-  Serial.println(digitalRead(3)); // input from button
   delay(200);
 }
