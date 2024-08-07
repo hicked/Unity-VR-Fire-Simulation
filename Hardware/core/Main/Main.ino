20fc8e4a91486475aeb2ba9f0ad4f4d0e2e602d1
Antoine Hickey <antoinehickey123@gmail.com>
2024-08-07 13:58:14 -0400

    arduino and control stuff

--- Hardware/core/Main/Main.ino
+++ Hardware/core/Main/Main.ino
@@ -1,37 +1,43 @@
-void setup() {
-  Serial.begin(19600);
-  pinMode(2, OUTPUT); // LED
-  pinMode(3, INPUT); // Left Wheel
-  pinMode(4, INPUT); // Right Wheel
-}
-
-
-const int BUFFER_SIZE = 50; // max size of data being pushed through
 float leftSpeed = 0;
 float rightSpeed = 0;
-float data[2] = {0, 0};
+
+byte LEDPin = 2;
+byte leftWheelPin = 3;
+byte rightWheelPin = 4;
+byte buttonPin = 5;
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LEDPin, OUTPUT); // LED
+  pinMode(leftWheelPin, INPUT); // Left Wheel
+  pinMode(rightWheelPin, INPUT); // Right Wheel
+  pinMode(buttonPin, INPUT); // temp button
+}
 
 void loop() {
   if (Serial.available() > 0) {
-    // leftSpeed = digitalRead(3);
-    // rightSpeed = digitalRead(4);
-    // data[0] = leftSpeed;
-    // data[1] = rightSpeed;
-    data[0] = 20;
-    data[1] = 30;
 
-    String data = String(leftSpeed) + "," + String(rightSpeed);
-
-    // Send the byte array through Serial
-    Serial.print(data);
-
-    // if (inputData[0] == '0') {
-    //   digitalWrite(2, LOW);
-    // }
-    // else if (inputData[0] == '1') {
-    //   digitalWrite(2, HIGH);
-    // }
+    // READING STUFF FROM UNITY
   }
+  // leftSpeed = digitalRead(3);
+  // rightSpeed = digitalRead(4);
+  // data[0] = leftSpeed;
+  // data[1] = rightSpeed;
+  if (!digitalRead(buttonPin)) { // for some reason the current is inverted here?
+    leftSpeed = 3;
+    rightSpeed = 3;
+  }
+  else {
+    leftSpeed = 0;
+    rightSpeed = 0;
+  }
+
+  String data = String(leftSpeed) + "," + String(rightSpeed);
+
+  // Send the byte array through Serial
+  Serial.println(data);
+
 
   delay(200);
 }
